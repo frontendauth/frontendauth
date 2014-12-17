9340a31d0302e0cf3dc9171a750cecc4c365b40b
AJ Zane <androiddreams@ajzane.com>
2014-12-16 16:38:25 -0800

    setup directories for build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,49 @@
+// Path variables
+// The assets and distribution paths
+var assets_path = "assets",
+    dist_path = "dist";
+// The `paths` object holds the source and distribution paths of all the directories
+// Edit once here and it will work everywhere
+// Most tasks only require the directory by default, but some need files.
+// Use paths.type.src model for directories, and paths.type.src_file model for files
 var paths = {
   styles: {
-    src : ['scss/**/*.scss','scss/**/*.sass'],
-    dist : 'dist/css'
+    src : assets_path+'/styles',
+    dist : dist_path+'/css',
+    srcfiles : [assets_path+'/styles/**/*.scss', assets_path+'/styles/**/*.sass'],
+    srcmainfile: assets_path+'/styles/app.scss',
+    distmainfile: dist_path+'/css/app.css' //currently broken
+  },
+  views: {
+    src: assets_path+'/views',
+    dist: dist_path,
+    srcfiles: [ assets_path+'/views', assets_path+'/views/**/*.html' ]
+  },
+  images: {
+    src: assets_path+'/images',
+    dist: dist_path+'/img',
+    srcfiles: [assets_path+'/images/**/*.jpg', assets_path+'/images/**/*.png', assets_path+'/images/**/*.gif', assets_path+'/images/**/*.svg'],
+  },
+  scripts: {
+    src: assets_path+'/scripts',
+    dist: dist_path+'/js',
+    srcfiles: [ assets_path+'/scripts/**/*.js', assets_path+'/scripts/**/*.coffee' ]
+  },
+  server: {
+    src: assets_path+'/server',
+    dist: dist_path,
+    srcfiles: assets_path+'/server/**/*',
   }
 };
 module.exports = function(grunt) {
+  var styles_dist_mainfile = paths.styles.distmainfile;
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    /**
+     * Sass
+     */
     sass: {
       options: {
         sourceMap: true,
@@ -22,28 +58,60 @@ module.exports = function(grunt) {
           outputStyle: 'compressed'
         },
         files: {
-          'css/app.css': 'scss/app.scss'
+          // styles_dist_mainfile: paths.styles.srcmainfile //this should work
+          'dist/css/app.css': paths.styles.srcmainfile
         }
       }
     },
 
-    views: {
-
+    /**
+     * Copy files that don't need to be compiled or modified
+     */
+    copy: {
+      //Images
+      images: {
+        files: [ {expand: true, cwd: paths.images.src, src: ['**'], dest: paths.images.dist} ],
+      },
+      //JavaScript
+      scripts: {
+        files: [ {expand: true, cwd: paths.scripts.src, src: ['**'], dest: paths.scripts.dist} ],
+      },
+      //Files at the root of the server (favicon, robots, htaccess, etc)
+      server: {
+        files: [ {expand: true, cwd: paths.server.src, src: ['**'], dest: paths.server.dist} ],
+      },
+      //Views
+      views: {
+        files: [ {expand: true, cwd: paths.views.src, src: ['**'], dest: paths.views.dist} ],
+      },
     },
 
     watch: {
       grunt: { files: ['Gruntfile.js'] },
 
-      sass: {
+      styles: {
         files: paths.styles.src,
         tasks: ['sass']
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
 
-  grunt.registerTask('build', ['sass']);
+  grunt.task.registerTask('images', ['copy:images']);
+  grunt.task.registerTask('server', ['copy:server']);
+  grunt.task.registerTask('scripts', ['copy:scripts']);
+  grunt.task.registerTask('styles', ['sass']);
+  grunt.task.registerTask('views', ['copy:views']);
+
+  grunt.registerTask('build', [
+    'images',
+    'server',
+    'scripts',
+    'styles',
+    'views'
+  ]);
   grunt.registerTask('default', ['build','watch']);
 }
