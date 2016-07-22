543af4bf3ab4419da2b64c5ed97f40b7afa4b678
AJ Zane <androiddreams@ajzane.com>
2016-07-21 20:29:44 -0700

    Adds routing

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,8 +1,34 @@
-import Vue from 'vue'
-import App from './App'
+var App = require('./App')
+var Vue = require('vue')
+var VueRouter = require('vue-router')
+var Foo = require('./components/Foo')
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+Vue.use(VueRouter)
+
+var Bar = Vue.extend({
+  template: '<p>This is bar!</p>'
 })
+
+// Create a router instance.
+// You can pass in additional options here, but let's
+// keep it simple for now.
+var router = new VueRouter()
+
+// Define some routes.
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// Vue.extend(), or just a component options object.
+// We'll talk about nested routes later.
+router.map({
+  '/foo': {
+    component: Foo
+  },
+  '/bar': {
+    component: Bar
+  }
+})
+
+// Now we can start the app!
+// The router will create an instance of App and mount to
+// the element matching the selector #app.
+router.start(App, '#app')
